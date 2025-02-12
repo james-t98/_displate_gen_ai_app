32c68e7ea82244f220d2118e5b34d5cd814f47ec
Jaime <jaime.tellie@gmail.com>
2025-02-12 13:24:56 +0000

    Designed grid for 4 images

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,31 @@
 import streamlit as st
+import os
+import glob
 from gcp_imagen import generate_image
 
 st.title("🎈 Jaime's app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
-if st.button("Generate an Image"):
-    st.write("Calling image generation")
-    generate_image()
+IMAGES_PATH = "daily_images"
+DAILY_IMAGES = glob.glob(f'{IMAGES_PATH}/*')
+IMAGES_NAMES = [os.path.basename(file) for file in DAILY_IMAGES]
+image_1, image_2, image_3, image_4 = False, False, False, False
+images = [image_1, image_2, image_3, image_4]
+
+def display_generated_images():
+    pass
+
+def get_selected_images():
+    for image in images:
+        if image == False:
+            st.write("Downloading ", image)
+
+col1 , col2 = st.columns(2)
+with col1:
+    if st.button("Generate an Image"):
+        display_generated_images()
+with col2:
+    if st.button("Download selected Images."):
+        get_selected_images()
 
 prompt = st.chat_input("Is there anything particular you are looking for today sir?")
 if prompt:
@@ -17,22 +34,20 @@ if prompt:
     else:
         st.write(f"User has sent the following prompt: {prompt}")
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
-    st.header("Image 1")
-    st.image("daily_images/image (5).png", caption="Sunrise by the mountains")
-    st.header("Image 4")
-    st.image("daily_images/image (5).png", caption="Sunrise by the mountains")
+    with st.container(border=True):
+        image_1 = st.checkbox("Image #1")
+        st.image(DAILY_IMAGES[0], caption=IMAGES_NAMES[0])
+    with st.container(border=True):
+        image_3 = st.checkbox("Image #3")
+        st.image(DAILY_IMAGES[1], caption=IMAGES_NAMES[1])
 
 with col2:
-    st.header("Image 2")
-    st.image("daily_images/image (5).png", caption="Sunrise by the mountains")
-    st.header("Image 5")
-    st.image("daily_images/image (5).png", caption="Sunrise by the mountains")
-
-with col3:
-    st.header("Image 3")
-    st.image("daily_images/image (5).png", caption="Sunrise by the mountains")
-    st.header("Image 6")
-    st.image("daily_images/image (5).png", caption="Sunrise by the mountains")
+    with st.container(border=True):
+        image_2 = st.checkbox("Image #2")
+        st.image(DAILY_IMAGES[2], caption=IMAGES_NAMES[2])
+    with st.container(border=True):
+        image_4 = st.checkbox("Image #4")
+        st.image(DAILY_IMAGES[3], caption=IMAGES_NAMES[3])
